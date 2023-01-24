0f63a44a903cff58ac875e572cfee6214db17b63
anmiroshnichenko <119445763+anmiroshnichenko@users.noreply.github.com>
2023-01-24 14:04:17 +0300

    Add files via upload

---
+++ avgfile1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+function avgfile () {
+	path=$1
+	file_count=0
+	total_size=0
+
+	if [[ ! -d $path ]]; then
+		echo "Directory $path does not exists" #>&2
+		exit 1
+	fi
+	for file in "$path"/*; 	do
+		if [[ -f $file ]]; then
+			(( total_size += $(stat -c%s "$file") ))
+			(( file_count++ ))
+		fi
+	done
+	if [[ $file_count -eq 0 ]]; then
+		echo "There is not files in $path"
+	else
+		echo "Average files size in $path is $(( total_size / file_count )) bytes"
+	fi
+}
+#vgfile "$1"
+#avgfile /tmp
+avgfile /home/miroshnichenko
+exit 0
